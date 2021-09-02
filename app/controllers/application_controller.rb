2405dbe4cbf00278c81913d2e3aaa97a0449f516
ayamiyokoi <didufd63hff@gmail.com>
2021-09-02 23:31:48 +0000

    [Add]fontawesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, if: :use_before_action?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  
+  def use_before_action?
+    true
+  end
+  
 end
