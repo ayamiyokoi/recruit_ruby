0c64fdbc0b57726e23e2139449da422f5598e19d
ayamiyokoi <didufd63hff@gmail.com>
2021-09-04 03:37:55 +0000

    [Add]すべての通過率関数

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -1,8 +1,8 @@
 class DatasController < ApplicationController
   def index
     # 書類選考
-    screening_process = Event.where(name: screening_process, user_id: current_user.id).count
-    screening_process_passed = Event.where(name: screening_process, user_id: current_user.id, is_passed: 'passed').count
+    screening_process = Event.where(name: 1, user_id: current_user.id).count
+    screening_process_passed = Event.where(name: 1, user_id: current_user.id, is_passed: 1).count
     if screening_process == 0
       @screening_process = 'データがありません'
     else
@@ -10,8 +10,8 @@ class DatasController < ApplicationController
     end
 
     # 一次選考
-    first_interview = Event.where(name: first_interview, user_id: current_user.id).count
-    first_interview_passed = Event.where(name: first_interview, user_id: current_user.id, is_passed: 'passed').count
+    first_interview = Event.where(name: 2, user_id: current_user.id).count
+    first_interview_passed = Event.where(name: 2, user_id: current_user.id, is_passed: 1).count
     if first_interview == 0
       @first_interview = 'データがありません'
     else
