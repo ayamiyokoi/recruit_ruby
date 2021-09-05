2e6884ef59197e6edef3b93e7e3960d0a284e67a
ayamiyokoi <didufd63hff@gmail.com>
2021-09-05 11:09:03 +0000

    [Add]validation

--- app/models/accept_condition.rb
+++ app/models/accept_condition.rb
@@ -2,4 +2,6 @@ class AcceptCondition < ApplicationRecord
   belongs_to :user
   has_many :checks, dependent: :destroy
   enum importance: { low: 0, medium: 1, high: 2 }
+  validates :content, {presence: true}
+  validates :importance, {presence: true}
 end
