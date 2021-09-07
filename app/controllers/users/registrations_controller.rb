cdc01cb0cc92c13f00c5a322c100d51890faaa78
ayamiyokoi <didufd63hff@gmail.com>
2021-09-07 04:53:12 +0000

    [Add]ゲストログイン機能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,6 +59,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  before_action :ensure_normal_user, only: :destroy
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+    end
+  end
+
+
   def use_before_action?
     false
   end
