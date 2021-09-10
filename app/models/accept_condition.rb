d8fd1ef8d416aac58c512ed383f21a3695e1ac12
ayamiyokoi <didufd63hff@gmail.com>
2021-09-10 00:33:37 +0000

    [Add]未登録部分表示機能

--- app/models/accept_condition.rb
+++ app/models/accept_condition.rb
@@ -1,5 +1,5 @@
 class AcceptCondition < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   has_many :checks, dependent: :destroy
   enum importance: { low: 0, medium: 1, high: 2 }
   validates :content, {presence: true}
--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,5 @@ class Company < ApplicationRecord
   has_many :checks, dependent: :destroy
   enum media: { unselected: 0, mynavi: 1, recnavi: 2, type: 3, doda: 4, green: 5, wantedly: 6, dmm: 7, other: 8 }
   validates :name, {presence: true}
+
 end
