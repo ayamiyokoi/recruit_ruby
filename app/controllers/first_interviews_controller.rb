efad2c5a7a62b6bb61b01817879cd2af5a402aee
ayamiyokoi <didufd63hff@gmail.com>
2021-09-22 06:45:31 +0000

    [Fix]データ範囲

--- app/controllers/first_interviews_controller.rb
+++ app/controllers/first_interviews_controller.rb
@@ -3,6 +3,8 @@ class FirstInterviewsController < ApplicationController
 
   def index
     @companies = Company.joins(:events).where(events: { name: 2, is_passed: 0 }, user_id: current_user.id, is_active: true).order(id: "DESC").page(params[:page]).per(PER_PAGE)
-    @companies_done = Company.joins(:events).where(events: { name: 2, is_passed: 0 }, user_id: current_user.id, is_active: false).order(id: "DESC").page(params[:page]).per(PER_PAGE)
+    @companies_pass = Company.joins(:events).where(events: { name: 2, is_passed: 1 }, user_id: current_user.id).order(id: "DESC").page(params[:page]).per(PER_PAGE)
+    @companies_drop = Company.joins(:events).where(events: { name: 2, is_passed: 2 }, user_id: current_user.id).order(id: "DESC").page(params[:page]).per(PER_PAGE)
+  
   end
 end
